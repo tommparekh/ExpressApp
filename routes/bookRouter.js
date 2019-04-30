34051342c5cb1b726af7a0a5cd622e5683c28bf0
Mihir Parekh <mihir.m.parekh@gmail.com>
2019-04-30 15:54:08 -0400

    Implemented middleware

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -3,31 +3,35 @@ const express = require('express');
 function routes(Book) {
   const bookRouter = express.Router();
 
+  // Middleware is placed using .use method. All it does is find the book from request (bookId). If found, set it in request, otherwise send error msg.
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        res.send(err);
+      }
+
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  })
+
   bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
-        if (err) {
-          res.send(err);
-        }
+    // book is already setup in req object by .use() middleware. Just return it.  
+    .get((req, res) => res.json(req.book))
 
-        return res.json(book);
-      });
-    })
+    // book is already setup in req object by .use() middleware. Just save it.
+    .put((req, res) => {
+      const {book} = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
 
-    .put((req,res) => {
-      Book.findById(req.params.bookId, (err, book) => {
-        if (err) {
-          res.send(err);
-        }
-
-        book.title = req.body.title;
-        book.author = req.body.author;
-        book.genre = req.body.genre;
-        book.read = req.body.read;
-
-        book.save();
-        return res.json(book);
-      });
+      book.save();
+      return res.json(book);
     });
 
   //curl -d '{"title": "Rich Dad Poor Dad","genre": "Financial Literacy","author": "Robart Kiosakey"}' -H "Content-Type: application/json" -X POST http://localhost:4000/api/books
@@ -61,7 +65,7 @@ function routes(Book) {
       });
     });
 
-    return bookRouter;
+  return bookRouter;
 }
 
 module.exports = routes;
