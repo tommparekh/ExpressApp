5a0be196d1ae174a34a0b8865aeb981c778294ac
Mihir Parekh <mihir.m.parekh@gmail.com>
2019-04-30 13:39:30 -0400

    Post data, create data in mongod

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
@@ -8,6 +9,16 @@ const port = process.env.PORT || 3000;
 
 
 const Book = require('./models/bookModel');
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use('/api', bookRouter);
+
+//app.use(express.json());
+//app.use(express.urlencoded())
+
+
 
 bookRouter.route('/books/:bookId')
 .get((req, res) => {
@@ -20,7 +31,21 @@ bookRouter.route('/books/:bookId')
   });
 });
 
+//curl -d '{"title": "Rich Dad Poor Dad","genre": "Financial Literacy","author": "Robart Kiosakey"}' -H "Content-Type: application/json" -X POST http://localhost:4000/api/books
+
+
+
+
+
 bookRouter.route('/books')
+  .post((request,response) => {
+    const book = new Book(request.body);
+    console.log(book);
+    book.save();
+ 
+    return response.status(201).json(book);
+  })
+
   .get((req, res) => {
   //  let query = {genre: 'Fantacy'};
   //  const {query} = req;
@@ -39,8 +64,6 @@ bookRouter.route('/books')
       return res.json(books);
     });
   });
-
-app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my API on port 4000!');
