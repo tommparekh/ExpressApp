9e9114c64cbf269fae2aa51949e7cdcf128078c3
Mihir Parekh <mihir.m.parekh@gmail.com>
2019-04-30 14:27:45 -0400

    Refactor router, added routes dir and bookRouter.js

--- app.js
+++ app.js
@@ -4,66 +4,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
-
-
 const Book = require('./models/bookModel');
 
+// Pass Book to routes function in ./routes/bookRouter.js
+const bookRouter = require('./routes/bookRouter') (Book);
 
-app.use(bodyParser.urlencoded({extended:false}));
+
+//Order of bodyParser and Router use must be preserved.
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api', bookRouter);
-
-//app.use(express.json());
-//app.use(express.urlencoded())
-
-
-
-bookRouter.route('/books/:bookId')
-.get((req, res) => {
-  Book.findById(req.params.bookId,(err, book) => {
-    if(err) {
-      res.send(err);
-    }
-
-    return res.json(book);
-  });
-});
-
-//curl -d '{"title": "Rich Dad Poor Dad","genre": "Financial Literacy","author": "Robart Kiosakey"}' -H "Content-Type: application/json" -X POST http://localhost:4000/api/books
-
-
-
-
-
-bookRouter.route('/books')
-  .post((request,response) => {
-    const book = new Book(request.body);
-    console.log(book);
-    book.save();
- 
-    return response.status(201).json(book);
-  })
-
-  .get((req, res) => {
-  //  let query = {genre: 'Fantacy'};
-  //  const {query} = req;
-  //  console.log(req);
-
-    const query = {};
-    if(req.query.genre) {
-      query.genre = req.query.genre;
-    }
-
-    Book.find(query,(err, books) => {
-      if(err) {
-        res.send(err);
-      }
-
-      return res.json(books);
-    });
-  });
 
 app.get('/', (req, res) => {
   res.send('Welcome to my API on port 4000!');
---
+++ routes/bookRouter.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+function routes(Book) {
+  const bookRouter = express.Router();
+
+  bookRouter.route('/books/:bookId')
+    .get((req, res) => {
+      Book.findById(req.params.bookId, (err, book) => {
+        if (err) {
+          res.send(err);
+        }
+
+        return res.json(book);
+      });
+    });
+
+  //curl -d '{"title": "Rich Dad Poor Dad","genre": "Financial Literacy","author": "Robart Kiosakey"}' -H "Content-Type: application/json" -X POST http://localhost:4000/api/books
+
+
+  bookRouter.route('/books')
+    .post((request, response) => {
+      const book = new Book(request.body);
+      console.log(book);
+      book.save();
+
+      return response.status(201).json(book);
+    })
+
+    .get((req, res) => {
+      //  let query = {genre: 'Fantacy'};
+      //  const {query} = req;
+      //  console.log(req);
+
+      const query = {};
+      if (req.query.genre) {
+        query.genre = req.query.genre;
+      }
+
+      Book.find(query, (err, books) => {
+        if (err) {
+          res.send(err);
+        }
+
+        return res.json(books);
+      });
+    });
+
+    return bookRouter;
+}
+
+module.exports = routes;
